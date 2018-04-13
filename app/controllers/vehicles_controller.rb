e3a2b813bec2fa0ffd375f2fae6bac3fbd363ae2
jal90 <mezzyjezze@gmail.com>
2018-04-13 17:34:52 -0400

    Git diff broke so I don't know what I'm adding

---
+++ app/controllers/vehicles_controller.rb
@@ -0,0 +1,51 @@
+class VehiclesController < ProtectedController
+  before_action :set_vehicle, only: %i[show update destroy]
+
+  # GET /vehicles
+  def index
+    @vehicles = current_user.vehicles
+
+    render json: @vehicles
+  end
+
+  # GET /vehicles/1
+  def show
+    render json: @vehicle
+  end
+
+  # POST /vehicles
+  def create
+    @vehicle = current_user.vehicles.build(vehicle_params)
+
+    if @vehicle.save
+      render json: @vehicle, status: :created, location: @vehicle
+    else
+      render json: @vehicle.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /vehicles/1
+  def update
+    if @vehicle.update(vehicle_params)
+      render json: @vehicle
+    else
+      render json: @vehicle.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /vehicles/1
+  def destroy
+    @vehicle.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_vehicle
+      @vehicle = current_user.vehicles.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def vehicle_params
+      params.require(:vehicle).permit(:make, :model, :year)
+    end
+end

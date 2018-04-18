e6bc2a3a2e2ac4585cb7374e983142dba77e59f5
jal90 <mezzyjezze@gmail.com>
2018-04-18 11:34:44 -0400

    Very important update to vehicle serializer
    Serializing out an array of works and then plucking the ids from them
    is necessary for my client to be able to get those ids. Ember expects
    an array of ids like that on the Vehicle model. Without that in the serializer
    Ember never receives that data

--- app/serializers/vehicle_serializer.rb
+++ app/serializers/vehicle_serializer.rb
@@ -1,3 +1,8 @@
 class VehicleSerializer < ActiveModel::Serializer
-  attributes :id, :make, :model, :year, :mileage, :user_id
+  attributes :id, :make, :model, :year, :mileage, :user_id, :works
+
+  # TODO: get a better understanding of exactly what's happening here
+  def works
+    object.works.pluck(:id)
+  end
 end
